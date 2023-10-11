b255967f49abbc4a0c90ce5b83252b66c3ee15e3
kvo52@kvo1 <kvo.52@mail.ru>
2023-10-11 14:30:45 +0300

    hw01_client and server_class ClientGUI

---
+++ src/main/java/server_window/ClientGUI.java
@@ -0,0 +1,146 @@
+package server_window;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class ClientGUI extends JFrame {
+    public static final int WIDTH = 500;
+    public static final int HEIGHT = 500;
+
+    private ServerWindow server;
+    private boolean connected;
+    private String name;
+
+    JTextArea log;
+    JTextField tfIPAddress, tfPort, tfLogin, tfMessage;
+    JPasswordField password;
+    JButton btnLogin, btnSend;
+    JPanel headerPanel;
+
+    ClientGUI(ServerWindow server){
+        this.server = server;
+
+        setSize(WIDTH, HEIGHT);
+        setResizable(false);
+        setTitle("Chat client");
+        setLocation(server.getX() - 500, server.getY());
+
+        createPanel();
+
+        setVisible(true);
+    }
+
+    public void answer(String text){
+        appendLog(text);
+    }
+
+    private void connectToServer() {
+        if (server.connectUser(this)){
+            appendLog("Вы успешно подключились!\n");
+            headerPanel.setVisible(false);
+            connected = true;
+            name = tfLogin.getText();
+            String log = server.getLog();
+            if (log != null){
+                appendLog(log);
+            }
+        } else {
+            appendLog("Подключение не удалось");
+        }
+    }
+
+    public void disconnectFromServer() {
+        if (connected) {
+            headerPanel.setVisible(true);
+            connected = false;
+            server.disconnectUser(this);
+            appendLog("Вы были отключены от сервера!");
+        }
+    }
+
+    public void message(){
+        if (connected){
+            String text = tfMessage.getText();
+            if (!text.equals("")){
+                server.message(name + ": " + text);
+                tfMessage.setText("");
+            }
+        } else {
+            appendLog("Нет подключения к серверу");
+        }
+
+    }
+
+    private void appendLog(String text){
+        log.append(text + "\n");
+    }
+
+    private void createPanel() {
+        add(createHeaderPanel(), BorderLayout.NORTH);
+        add(createLog());
+        add(createFooter(), BorderLayout.SOUTH);
+    }
+
+    private Component createHeaderPanel(){
+        headerPanel = new JPanel(new GridLayout(2, 3));
+        tfIPAddress = new JTextField("198.168.101.1");
+        tfPort = new JTextField("9399");
+        tfLogin = new JTextField("Kira Naytly");
+        password = new JPasswordField("123456");
+        btnLogin = new JButton("Enter");
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                connectToServer();
+            }
+        });
+
+        headerPanel.add(tfIPAddress);
+        headerPanel.add(tfPort);
+        headerPanel.add(new JPanel());
+        headerPanel.add(tfLogin);
+        headerPanel.add(password);
+        headerPanel.add(btnLogin);
+
+        return headerPanel;
+    }
+
+    private Component createLog(){
+        log = new JTextArea();
+        log.setEditable(false);
+        return new JScrollPane(log);
+    }
+
+    private Component createFooter() {
+        JPanel panel = new JPanel(new BorderLayout());
+        tfMessage = new JTextField();
+        tfMessage.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == '\n'){
+                    message();
+                }
+            }
+        });
+        btnSend = new JButton("send");
+        btnSend.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                message();
+            }
+        });
+        panel.add(tfMessage);
+        panel.add(btnSend, BorderLayout.EAST);
+        return panel;
+    }
+
+    @Override
+    public int getDefaultCloseOperation() {
+        disconnectFromServer();
+        return super.getDefaultCloseOperation();
+    }
+}
